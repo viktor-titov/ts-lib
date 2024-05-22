b5f4f59b5f4bf1678df2ebe9664d61cd6faf4936
Viktor Titov <nulltomato@gmail.com>
2024-05-22 23:42:26 +0300

    init

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require("webpack");
+const path = require("path");
+
+module.exports = {
+    entry: path.join(__dirname, "src", "index.ts"),
+    experiments: {
+        outputModule: true,
+    },
+    output: {
+        path: path.resolve(__dirname, '_bundles'),
+        filename: '[name].js',
+        library: {
+            type: 'module'
+        },
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js"],
+    },
+    devtool: 'source-map',
+    module: {
+        strictExportPresence: true,
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        cacheDirectory: true, // Using a cache to avoid of recompilation
+                    },
+                },
+            },
+        ],
+    },
+}
